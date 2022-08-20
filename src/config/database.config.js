d672c9ed88da9937e149e7acbf1d2dbbf5b58842
Jesus <jcarranza@joonik.com>
2022-08-20 13:15:12 -0500

    se agrego metodo para actualizar

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -4,10 +4,15 @@ const { Sequelize } = require('sequelize');
 
 // password cpanel bn&c;zvWV%;i
 // const db = new Sequelize('hncjeodg_hdlatino_app_post', 'root', '', {
-const db = new Sequelize('hncjeodg_hdlatino_app_post', 'hncjeodg_hdlatino_app_post', 'bn&c;zvWV%;i', {
-	host: 'localhost',
-	dialect: 'mysql'
-	/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-});
+	const db = new Sequelize(
+		'hncjeodg_hdlatino_app_post',
+		'hncjeodg_hdlatino_app_post',
+		'bn&c;zvWV%;i',
+		{
+			host: 'localhost',
+			dialect: 'mysql'
+			/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+		}
+	);
 
 module.exports = db;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -18,31 +18,11 @@ class Users {
 	getRouter = () => this.router;
 
 	routes() {
-		// this.getUser('/:id');
 		this.getUsers('/');
 		this.postCreate('/');
 		this.getUser('/:username');
-		// this.postCreateUser('/crear');
-		// this.putUpdateUser('/:id');
+		this.putUpdate('/:id');
 	}
-	// getUser(route) {
-	// 	this.router.get(route, async (req) => {
-	// 		const id = req.request.params.id;
-	// 		try {
-	// 			const user = await User.findOne({ _id: id });
-	// 			if (!user) {
-	// 				return res.status(404).send('User not found');
-	// 			}
-
-	// 			req.response.set('content-type', mimeType);
-	// 			req.response.status = 200;
-	// 			req.body = user;
-	// 		} catch (error) {
-	// 			req.response.status = 500;
-	// 			req.body = { errors: 'Bad request', error };
-	// 		}
-	// 	});
-	// }
 
 	getUsers(route) {
 		this.router.get(route, async (req) => {
@@ -78,6 +58,36 @@ class Users {
 			}
 		});
 	}
+	putUpdate(route) {
+		this.router.put(route, async (req) => {
+			const { username, password, alias, id_wordpress, id_paste } =
+				req.request.body;
+			const { id } = req.request.params;
+
+			if (!id) {
+				return req.status(404).send('User not found');
+			}
+			try {
+				const users = await User.upsert({
+					id,
+					username,
+					password,
+					alias,
+					id_wordpress,
+					id_paste
+				});
+				if (!users) {
+					return req.status(404).send('User not found');
+				}
+
+				req.response.status = 200;
+				req.body = users;
+			} catch (error) {
+				req.response.status = 500;
+				req.body = { errors: 'Bad request', error };
+			}
+		});
+	}
 	postCreate(route) {
 		this.router.post(route, async (req) => {
 			const { username, password, alias, id_wordpress, id_paste } =
@@ -102,51 +112,6 @@ class Users {
 			}
 		});
 	}
-
-	// postCreateUser(route) {
-	// 	this.router.post(route, async (req) => {
-	// 		const { nombre, password } = req.request.body;
-	// 		try {
-	// 			const user = new User({
-	// 				nombre,
-	// 				password
-	// 			});
-	// 			const result = await user.save();
-	// 			console.log(result);
-	// 			req.response.status = 200;
-	// 			req.body = result;
-	// 		} catch (error) {
-	// 			req.response.status = 500;
-	// 			req.body = { errors: 'Bad request', error };
-	// 		}
-	// 	});
-	// }
-
-	// putUpdateUser(route) {
-	// 	this.router.put(route, async (req) => {
-	// 		const { id } = req.request.params;
-	// 		const { nombre, password } = req.request.body;
-	// 		try {
-	// 			const user = await User.updateOne(
-	// 				{
-	// 					_id: id
-	// 				},
-	// 				{
-	// 					$set: {
-	// 						nombre,
-	// 						password
-	// 					}
-	// 				}
-	// 			);
-
-	// 			req.response.status = 200;
-	// 			req.body = user;
-	// 		} catch (error) {
-	// 			req.response.status = 500;
-	// 			req.body = { errors: 'Bad request', error };
-	// 		}
-	// 	});
-	// }
 }
 
 const routerUserss = new Users();
